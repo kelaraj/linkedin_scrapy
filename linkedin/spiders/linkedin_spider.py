a6974da5509140d8fe51b7f98b040c8bb150eb7e
Dev Mehta <dpmehta02@gmail.com>
2013-11-08 11:43:10 -0800

    update gitignore

--- linkedin/spiders/linkedin_spider.py
+++ linkedin/spiders/linkedin_spider.py
@@ -1,13 +1,21 @@
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from linkedin.items import LinkedinItem
 
 class DmozSpider(BaseSpider):
-    name = "linkedin"
-    allowed_domains = ["linkedin.com"]
-    start_urls = [
-        "http://www.linkedin.com/directory/people-%s" % alphanum for alphanum in 
-        "abcdefghijklmnopqrstuvwxyz"
-        ]
+  name = "linkedin"
+  allowed_domains = ["linkedin.com"]
+  start_urls = ["http://www.linkedin.com/directory/people-%s" % alphanum 
+                for alphanum in "abcdefghijklmnopqrstuvwxyz"]
 
-    def parse(self, response):
-        filename = response.url.split("/")[-1]
-        open(filename, 'wb').write(response.body)
+  def parse(self, response):
+    hxs = HtmlXPathSelector(response)
+    sites = hxs.select('//ul[2]/li')
+    items = []
+#    for site in sites:
+#      item = LinkedinItem()
+#      item['name'] = site.select('a/text()').extract()
+#      items.append(item)
+
+    filename = "./testdata/" + response.url.split("/")[-1]
+    open(filename, 'wb').write(response.body)
