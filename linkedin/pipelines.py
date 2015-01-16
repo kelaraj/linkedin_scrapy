0f60e6899f5fd144e5ef22af49d2722359949d55
dpmehta02 <dpmehta02@gmail.com>
2015-01-15 18:35:23 -0800

    fix formatting

--- linkedin/pipelines.py
+++ linkedin/pipelines.py
@@ -1,30 +1,9 @@
-# Define your item pipelines here
+# Define item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 class LinkedinPipeline(object):
     def process_item(self, item, spider):
+        # TODO: dedupe records, clean up HTML
         return item
-
-# Add Mongodb pipeline, dedupe
-
-# Clean HTML
-# strip/clean headline_title, locality
-
-# DUPLICATE CHECK
-
-#from scrapy import signals
-#from scrapy.exceptions import DropItem
-
-#class LinkedinPipeline(object):
-
-#    def __init__(self):
-#        self.ids_seen = set()
-
-#    def process_item(self, item, spider):
-#        if item['id'] in self.ids_seen:
-#            raise DropItem("Duplicate item found: %s" % item)
-#        else:
-#            self.ids_seen.add(item['id'])
-#            return item
--- linkedin/spiders/linkedin_spider.py
+++ linkedin/spiders/linkedin_spider.py
@@ -4,74 +4,73 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 from linkedin.items import LinkedinItem
 
-# compile to C using Cython if processing speed becomes a constraint
 
 class LinkedinSpider(CrawlSpider):
-  
-  """
-  Define the crawler's start urls, set its follow rules, parse HTML
-  and assign values to an item. Processing occurs in ../pipelines.py
-  """
 
-  name = "linkedin"
-  allowed_domains = ["linkedin.com"]
-  
-  # TODO: uncomment following lines for full spidering
-  '''
-  centilist_one = (i for i in xrange(1,100))
-  centilist_two = (i for i in xrange(1,100))
-  centilist_three = (i for i in xrange(1,100))
-  start_urls = ["http://www.linkedin.com/directory/people-%s-%d-%d-%d" 
-                % (alphanum, num_one, num_two, num_three) 
-                  for alphanum in "abcdefghijklmnopqrstuvwxyz"
-                  for num_one in centilist_one
-                  for num_two in centilist_two
-                  for num_three in centilist_three
-                ]
-  '''
-  # temporary start url, remove for production
-  start_urls = ["http://www.linkedin.com/directory/people-a-23-23-2"]
-  # TODO: allow /in/name urls too? (LinkedIn custom URLs)
-  rules = (Rule(SgmlLinkExtractor(allow=('\/pub\/.+', ))
-                , callback='parse_item'),
-          )
+    """
+    Define the crawler's start URIs, set its follow rules, parse HTML
+    and assign values to an item. Processing occurs in ../pipelines.py
+    """
 
-  def parse_item(self, response):
-    if response:
-      hxs = HtmlXPathSelector(response)
-      item = LinkedinItem()
-      # is this the best way to check that I'm scraping the right page?
-      item['full_name'] = hxs.select('//*[@id="name"]/span/span/text()').extract()
-      if not item['full_name']:
-        # recursively parse list of duplicate profiles
-        # NOTE: Results page only displays 25 of possibly many more names;
-        # LinkedIn requests authentication to see the rest. Need to resolve
-        # Fake account and log-in?
-        
-        # TODO: add error checking here to ensure I'm getting the right links
-        # and links from "next>>" pages
-        multi_profile_urls = hxs.select('//*[@id="result-set"]/li/h2/strong/ \
+    name = "linkedin"
+    allowed_domains = ["linkedin.com"]
+
+    # Uncomment the following lines for full spidering
+    '''
+    centilist_one = (i for i in xrange(1,100))
+    centilist_two = (i for i in xrange(1,100))
+    centilist_three = (i for i in xrange(1,100))
+    start_urls = ["http://www.linkedin.com/directory/people-%s-%d-%d-%d" 
+                  % (alphanum, num_one, num_two, num_three) 
+                    for alphanum in "abcdefghijklmnopqrstuvwxyz"
+                    for num_one in centilist_one
+                    for num_two in centilist_two
+                    for num_three in centilist_three
+                  ]
+    '''
+    
+    # Temporary start_urls for testing; remove and use the above start_urls in production
+    start_urls = ["http://www.linkedin.com/directory/people-a-23-23-2"]
+    # TODO: allow /in/name urls too?
+    rules = (Rule(SgmlLinkExtractor(allow=('\/pub\/.+')), callback='parse_item'))
+
+    def parse_item(self, response):
+        if response:
+            hxs = HtmlXPathSelector(response)
+            item = LinkedinItem()
+            # TODO: is this the best way to check that we're scraping the right page?
+            item['full_name'] = hxs.select('//*[@id="name"]/span/span/text()').extract()
+            if not item['full_name']:
+
+                # recursively parse list of duplicate profiles
+                # NOTE: Results page only displays 25 of possibly many more names;
+                # LinkedIn requests authentication to see the rest. Need to resolve
+
+                # TODO: add error checking here to ensure I'm getting the right links
+                # and links from "next>>" pages
+                multi_profile_urls = hxs.select('//*[@id="result-set"]/li/h2/strong/ \
                                           a/@href').extract()
-        for profile_url in multi_profile_urls:
-          yield Request(profile_url, callback=self.parse_item)
-      else:
-        # add meta fields (date crawled/updated, etc)
-        item['first_name'] = item['full_name'][0]
-        item['last_name'] = item['full_name'][1]
-        item['full_name'] = hxs.select('//*[@id="name"]/span/span/text()').extract()
-        item['headline_title'] = hxs.select('//*[@id="member-1"]/p/text() \
-                                            ').extract()
-        item['locality'] = hxs.select('//*[@id="headline"]/dd[1]/span/text() \
-                                            ').extract()
-        item['industry'] = hxs.select('//*[@id="headline"]/dd[2]/text() \
-                                            ').extract()
-        item['current_roles'] = hxs.select('//*[@id="overview"]/dd[1]/ul/li/ \
-                                                          text()').extract()
-        # TODO: dynamically check for header of field, assign to object
-        # via variable
-        if hxs.select('//*[@id="overview"]/dt[2]/text()\
-                                ').extract() == [u' \n       Education\n    ']:
-          item['education_institutions'] = hxs.select('//*[@id="overview"]/\
-                                                dd[2]/ul/li/text()').extract()
-        # for debugging
-        print item
+                for profile_url in multi_profile_urls:
+                    yield Request(profile_url, callback=self.parse_item)
+            else:
+                item['first_name'], 
+                item['last_name'], 
+                item['full_name'],
+                item['headline_title'],
+                item['locality'], 
+                item['industry'], 
+                item['current_roles'] = item['full_name'][0], 
+                                        item['full_name'][1], 
+                                        hxs.select('//*[@id="name"]/span/span/text()').extract(),
+                                        hxs.select('//*[@id="member-1"]/p/text()').extract(),
+                                        hxs.select('//*[@id="headline"]/dd[1]/span/text()').extract(),
+                                        hxs.select('//*[@id="headline"]/dd[2]/text()').extract(),
+                                        hxs.select('//*[@id="overview"]/dd[1]/ul/li/text()').extract()
+                # TODO: add metadata fields
+
+                if hxs.select('//*[@id="overview"]/dt[2]/text()').extract() == [u' \n       Education\n    ']:
+                    item['education_institutions'] = hxs.select('//*[@id="overview"]/dd[2]/ul/li/text()').extract()
+                print item
+        else:
+            print "Uh oh, no response."
+            return
